bbdfd59693752a8d62342424bb26d47ca5aba321
muse9312 <muse9312@naver.com>
2022-08-04 16:01:43 +0900

    rename blog

--- blog/src/App.js
+++ blog/src/App.js
@@ -4,7 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <div className="black-nav">
+        <h4>블로그</h4>
+      </div>
+      <div className="list">
+        <p>강남 맛집 리뷰</p>
+      </div>
     </div>
   );
 }
